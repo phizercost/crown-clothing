c25ed6e7cbf9c022962a416d1b50f1214b00ca35
phizercost <phizer.development@gmail.com>
2019-11-12 10:19:05 +0200

    moved shop directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './redux/store';
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
-  <Provider store = { store }>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
-  
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import { persistStore} from 'redux-persist';
 
 const middleWares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store, persistor };
